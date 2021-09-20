fc92144f90a2112ed7baa2b5c4a124176847f2e3
Fatemeh ghodratian <f.ghodratian96@gmail.com>
2021-09-20 19:49:39 +0430

    scripts for checking form validation was added

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.getElementById("searchBtn").onclick = function() {searchFunction()};
+
+function searchFunction() {
+    const region=document.getElementById("regionSelect").value ;
+    const checkIn=document.getElementById("checkIn").value ;
+    const checkOut=document.getElementById("checkOut").value ;
+    var checkInDate = new Date(checkIn);
+    var checkOutDate = new Date(checkOut);
+    var currentDate = new Date();
+    console.log(region,checkIn,checkOut)
+        if(region=== "Select a region" && checkIn==0 && checkOut==0)
+    {
+        alert("please Select a 'region' and choose 'check in' and 'check out' date");
+    }
+    else if (checkIn==0 && checkOut==0){
+        alert("please choose 'check in' and 'check out' date");
+    }
+    else if (region=== "Select a region"|| checkIn==0 || checkOut==0){
+        alert("please Select a 'region' and it seems that you didn't choose 'check in' or 'check out' date");
+    }
+    else if (checkIn==0 || checkOut==0){
+        alert("it seems that you didn't choose 'check in' or 'check out' date");
+    }
+    else if (currentDate.setHours(0, 0, 0, 0)== checkInDate.setHours(0, 0, 0, 0) || currentDate.setHours(0, 0, 0, 0)== checkOutDate.setHours(0, 0, 0, 0)){
+        alert("you can't choose 'Today' for 'check in' or 'check out");
+    }
+    else{
+        alert("OK, lets Travel With Us");
+    }
+}
