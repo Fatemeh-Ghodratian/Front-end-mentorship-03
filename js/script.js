fc3811486e5a162d62cdd15d455738bb5bc17e8d
Fatemeh ghodratian <f.ghodratian96@gmail.com>
2021-09-20 22:34:04 +0430

    additional alert(bug) fixed

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function searchFunction() {
     var checkInDate = new Date(checkIn);
     var checkOutDate = new Date(checkOut);
     var currentDate = new Date();
-        if(region=== "Select a region" && checkIn==0 && checkOut==0)
+        if(region == "Select a region" && checkIn==0 && checkOut==0)
     {
         alert("please Select a 'region' and choose 'check in' and 'check out' date");
     }
@@ -23,14 +23,14 @@ function searchFunction() {
     else if (checkIn==0 ||  checkOut==0){
         alert("it seems that you didn't choose 'check in' or 'check out' date");
     }
-    if(region=== "Select a region")
+    if(region == "Select a region")
     {
         alert("please Select a 'region'");
     }
     else if (currentDate.setHours(0, 0, 0, 0)== checkInDate.setHours(0, 0, 0, 0) || currentDate.setHours(0, 0, 0, 0)== checkOutDate.setHours(0, 0, 0, 0)){
         alert("you can't choose 'Today' for 'check in' or 'check out");
     }
-    else{
+    else if(region != "Select a region" && checkIn!=0 && checkOut!=0) {
         console.log(region);
         alert("OK, lets Travel With Us");
     }
